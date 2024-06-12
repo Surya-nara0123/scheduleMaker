8798313cd6cb469e85f9dd33cc4f7ff0b4af5ecd
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-06-13 01:26:43 +0530

    shit

--- timetable/app/timetable/test/page.tsx
+++ timetable/app/timetable/test/page.tsx
@@ -5,7 +5,18 @@ import React, { useState } from "react";
 import Sidebar from "../../Components/Sidebar";
 import SVGStar from "../../Components/star";
 export default function Table() {
+  const [year, setYear] = useState("");
+  const [section, setSection] = useState("");
   const [year1, setYear1] = useState(["8.10-9.00", "9.00-9.50", "break", "10.10-11.00", "11.00-11.50","Lunch", "12.50-1.40", "1.40-2.30", "break", "2.40-3.30"]);
+
+  const generateTimeTable = () => {
+    if (year == "1st"){
+      setYear1(["8.10-9.00", "9.00-9.50", "break", "10.10-11.00", "11.00-11.50","Lunch", "12.50-1.40", "1.40-2.30", "break", "2.40-3.30"]);
+    } else {
+      setYear1(["8.10-9.00", "9.00-9.50", "9.50-10.40", "10.40-11.30","Lunch", "12.50-1.40", "1.40-2.30", "break", "2.40-3.30"]);
+    }
+    console.log(year, section);
+  };
   return (
     <main className="min-h-screen bg-[#B4D2E7]">
       <div className="flex">
@@ -16,6 +27,32 @@ export default function Table() {
             <div className="bg-white h-0.5 w-1/2 mt-1"></div>
           </div>
           <div className="m-5 items-center justify-center p-3 ml-10">
+            <select
+              className="bg-white rounded-lg p-2 mr-2"
+              onChange={(e) => {
+                setYear(e.target.value);
+              }}
+            >
+              <option defaultChecked>Select year</option>
+              <option>1st</option>
+              <option>2nd</option>
+            </select>
+            <select
+              className="bg-white rounded-lg p-2 mr-2"
+              onChange={(e) => {
+                setSection(e.target.value);
+              }}
+            >
+              <option defaultChecked>Select section</option>
+              <option>2st Year B.Tech CyberSecurity</option>
+              <option>2st Year B.Tech AI/DS A</option>
+              <option>AI/DS B</option>
+              <option>AI/DS C</option>
+              <option>IoT A</option>
+              <option>IoT B</option>
+              <option>Bcom PA</option>
+              <option>Bcom</option>
+            </select>
             <button className="bg-green-500 justfiy-center items-center rounded-lg p-2" onClick={generateTimeTable}>
               Generate timetable
             </button>
