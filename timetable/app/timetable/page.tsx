3ec784862a5bf9e9971db86cdde0548bf39b2962
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-06-12 17:49:21 +0530

    merge conflit solved

--- timetable/app/timetable/page.tsx
+++ timetable/app/timetable/page.tsx
@@ -848,13 +848,8 @@ export default function Table() {
     // console.log();
     // console.log(data2);
     let temp = {"1st Year B_Tech IoT Section A":data["1st Year B_Tech IoT Section A"]}
-<<<<<<< Updated upstream
     console.log(temp);
     generatePDF(data);
-=======
-    // console.log(temp);
-    // generatePDF(data2);
->>>>>>> Stashed changes
   };
   return (  
     <main className="min-h-screen bg-[#B4D2E7]">
