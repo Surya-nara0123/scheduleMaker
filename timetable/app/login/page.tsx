1fa0104d58eee17fc9a3fb618d709270e88d4cc1
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-06-13 23:08:34 +0530

    basic UI for login

--- timetable/app/login/page.tsx
+++ timetable/app/login/page.tsx
@@ -1,41 +1,51 @@
-'use client'
-import pocketbase from 'pocketbase';
-import { useState } from 'react';
+"use client";
+import pocketbase from "pocketbase";
+import { useState } from "react";
+import Sidebar from "../Components/Sidebar";
+import snu02 from "../Components/snu-02.jpg";
+import Image from "next/image";
 
-const pb = new pocketbase('https://snuc.pockethost.io');
+const pb = new pocketbase("https://snuc.pockethost.io");
 
 function Login() {
-    const handleLogin = async () => {
-        const record = await pb.collection('users').getFirstListItem('username="Frenzy"', {
-            expand: 'relField1,relField2.subRelField',
-        });
-        console.log(record);
-    }        
-    const [user, setUser] = useState({email: '', password: ''});
-    return ( 
-        <main>
-            <div>
-                <h1>Login</h1>
-                <div>
-            <input type="text" placeholder="email"
-            onChange={
-              (e) => {
-                setUser({...user, email: e.target.value});
-              }
-            
-            } />
-            <input type="password" placeholder="Password" 
-            onChange={
-              (e) => {
-                setUser({...user, password: e.target.value});
-              }
-            
-            } />
-                    <button type="submit" onClick={handleLogin}>Login</button>
-                    </div>
-            </div>
-        </main>
-     );
+  const handleLogin = async () => {
+    const record = await pb
+      .collection("users")
+      .getFirstListItem('username="Frenzy"', {
+        expand: "relField1,relField2.subRelField",
+      });
+    console.log(record);
+  };
+  const [user, setUser] = useState({ email: "", password: "" });
+  return (
+    <main className="flex w-screen h-screen items-center justify-center">
+      <Image src={snu02} alt="SNU" layout="fill" objectFit="cover" />
+      <div className="z-10 bg-[#054AA1] p-10 items-center flex flex-col  rounded-2xl">
+        <h1>Login</h1>
+        <div className="flex-col flex">
+          <input
+          className="m-3 rounded-md p-3"
+            type="text"
+            placeholder="email"
+            onChange={(e) => {
+              setUser({ ...user, email: e.target.value });
+            }}
+          />
+          <input
+          className="m-3 rounded-md p-3"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setUser({ ...user, password: e.target.value });
+            }}
+          />
+          <button type="submit" onClick={handleLogin}>
+            Login
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Login;
