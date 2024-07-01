9f49a5308037e0149b1a1421d23046ac4a9a63b9
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-07-01 14:37:43 +0530

    fixed the shit hotaru left for me thank you hotaru

--- timetable/app/timetablegen/page.jsx
+++ timetable/app/timetablegen/page.jsx
@@ -556,82 +556,108 @@ export default function Table() {
                     )}
                   </div>
                 </div>
-
-                <div className="md:mt-12 md:ml-12 flex flex-col items-center bg-white p-4 rounded-lg">
-                  <div className="font-black mr-auto ml-2 text-2xl mb-3">
-                    {(currentYear + " B_Tech " + currentSection).replace(
-                      "B_Tech",
-                      "B.Tech",
-                    )}
-                  </div>
-                  <div className="flex items-center mb-4 rounded-lg px-2">
-                    <div className=" flex flex-col items-center rounded-lg">
-                      <div className="md:w-24 md:h-8 w-12 h-4 flex text-[7px] md:text-sm items-center justify-center border bg-[#909090] rounded-lg mr-1">
-                        Slot
-                      </div>
-                      <div className="md:w-24 md:h-8 w-12 h-4 flex text-[7px] md:text-sm items-center justify-center border bg-[#909090] rounded-lg mr-1">
-                        Day
-                      </div>
-                    </div>
-                    <div className="flex gap-1">
-                      {currentYear == "1st Year"
-                        ? year1.map((slot, index) => (
-                            <div
-                              key={index}
-                              className="md:w-24 md:h-16 flex items-center w-12 h-8 text-[7px] md:text-sm justify-center bg-[#bfc0c0] rounded-lg "
-                            >
-                              {slot}
+                  {Object.keys(timetableData).map(
+                  (dataa, index) =>
+                    dataa == (currentYear + " B_Tech " + currentSection) && (
+                      <div className="md:mt-6 md:ml-14 flex flex-col items-center bg-white p-4 rounded-lg">
+                        <div className="font-black mr-auto ml-2 text-2xl mb-3">
+                          {dataa.replace("B_Tech", "B.Tech")}
+                        </div>
+                        <div className="flex items-center mb-4 rounded-lg px-2">
+                          <div className=" flex flex-col items-center rounded-lg">
+                            <div className="md:w-24 md:h-8 w-12 h-4 flex text-[7px] md:text-sm items-center justify-center border bg-[#909090] rounded-lg mr-1">
+                              Slot
                             </div>
-                          ))
-                        : [
-                            "8.10-9.00",
-                            "9.00-9.50",
-                            "9.50-10.40",
-                            "Break",
-                            "11.00-11.50",
-                            "11.50-12.40",
-                            "Lunch",
-                            "1.40-2.30",
-                            "Break",
-                            "2.40-3.30",
-                          ].map((slot, index) => (
-                            <div
-                              key={index}
-                              className="w-12 h-8 md:w-24 md:h-16 flex items-center text-[7px] md:text-sm justify-center bg-[#bfc0c0] rounded-lg "
-                            >
-                              {slot}
+                            <div className="md:w-24 md:h-8 w-12 h-4 flex text-[7px] md:text-sm items-center justify-center border bg-[#909090] rounded-lg mr-1">
+                              Day
                             </div>
-                          ))}
-                    </div>
-                  </div>
-                  <div className="flex">
-                    <div className="flex flex-col gap-1 rounded-lg mr-1">
-                      {["Mon", "Tue", "Wed", "Thu", "Fri"].map((day, index) => (
-                        <>
-                          <div
-                            key={index}
-                            className="w-12 h-8 md:w-24 md:h-16 flex text-[7px] md:text-sm items-center justify-center border bg-[#bfc0c0] rounded-lg"
-                          >
-                            <div>{day}</div>
                           </div>
-
-                          <div
-                            className="grid grid-rows-5 rounded-lg gap-1 grid-flow-col"
-                            key={index + 7}
-                          >
-                            {table[
-                              currentYear + " B_Tech " + currentSection
-                            ].map((_, colIndex) => (
-                              <div className="w-12 h-8 md:w-24 md:h-16 bg-[#dfdfdf] rounded-lg justfiy-center items-center flex text-center text-[5px] md:text-[10px] overflow-auto">
-                                {table[currentYear + ' B_Tech ' + currentSection][index][colIndex]}
-                              </div>
-                            ))}
+                          <div className="flex gap-1">
+                            {index <= 5
+                              ? year1.map((slot, index) => (
+                                  <div
+                                    key={index}
+                                    className="md:w-24 md:h-16 flex items-center w-12 h-8 text-[7px] md:text-sm justify-center bg-[#bfc0c0] rounded-lg "
+                                  >
+                                    {slot}
+                                  </div>
+                                ))
+                              : [
+                                  "8.10-9.00",
+                                  "9.00-9.50",
+                                  "9.50-10.40",
+                                  "break",
+                                  "11.00-11.50",
+                                  "11.50-12.40",
+                                  "Lunch",
+                                  "1.40-2.30",
+                                  "break",
+                                  "2.40-3.30",
+                                ].map((slot, index) => (
+                                  <div
+                                    key={index}
+                                    className="w-12 h-8 md:w-24 md:h-16 flex items-center text-[7px] md:text-sm justify-center bg-[#bfc0c0] rounded-lg "
+                                  >
+                                    {slot}
+                                  </div>
+                                ))}
                           </div>
-                        </>
-                      ))}
-                    </div>
-                    <div className="grid grid-rows-5 rounded-lg gap-1 grid-flow-col"></div>
-                  </div>
+                        </div>
+                        <div className="flex">
+                          <div className="flex flex-col gap-1 rounded-lg mr-1">
+                            {["Mon", "Tue", "Wed", "Thu", "Fri"].map(
+                              (day, index) => (
+                                <div
+                                  key={index}
+                                  className="w-12 h-8 md:w-24 md:h-16 flex text-[7px] md:text-sm items-center justify-center border bg-[#bfc0c0] rounded-lg"
+                                >
+                                  {day}
+                                </div>
+                              ),
+                            )}
+                          </div>
+                          <div className="grid grid-rows-5 rounded-lg gap-1 grid-flow-col">
+                            {Array.from({ length: year1.length }).map(
+                              (_, colIndex) => (
+                                <>
+                                  {Array.from({ length: 5 }).map(
+                                    (_, rowIndex) => (
+                                      <div
+                                        key={colIndex + rowIndex}
+                                        className="w-12 h-8 md:w-24 md:h-16 bg-[#dfdfdf] rounded-lg justfiy-center items-center flex text-center text-[5px] md:text-[10px] overflow-auto"
+                                      >
+                                        {timetableData[dataa][rowIndex][
+                                          colIndex
+                                        ] != "b" &&
+                                        timetableData[dataa][rowIndex][
+                                          colIndex
+                                        ] != "l" ? (
+                                          <div className="text-center w-full h-full items-center justify-center flex font-bold">
+                                            {
+                                              timetableData[dataa][rowIndex][
+                                                colIndex
+                                              ]
+                                            }
+                                          </div>
+                                        ) : timetableData[dataa][rowIndex][
+                                            colIndex
+                                          ] == "b" ? (
+                                          <div className="text-center w-full h-full items-center justify-center flex font-bold">
+                                            Break
+                                          </div>
+                                        ) : (
+                                          <div className="text-center w-full h-full items-center justify-center flex font-bold">
+                                            Lunch
+                                          </div>
+                                        )}
+                                      </div>
+                                    ),
+                                  )}
+                                </>
+                              ),
+                            )}
+                          </div>
+                        </div>
                   <button 
                     className="bg-blue-500 p-3 rounded-lg ml-auto mt-3 mr-2"
                     onClick={() => {
@@ -640,7 +666,9 @@ export default function Table() {
                   >
                     Save
                   </button>
-                </div>
+                      </div>
+                    ),
+                )}
               </>
             )}
           </div>
